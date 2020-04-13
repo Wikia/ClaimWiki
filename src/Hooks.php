594d25b731770cd81143a30430e6035cb2744177
Alexia E. Smith <washuu@gmail.com>
2020-04-13 12:59:05 -0500

    Line length.

--- src/Hooks.php
+++ src/Hooks.php
@@ -29,7 +29,8 @@ class Hooks {
 	 * @return void
 	 */
 	public static function onRegistration() {
-		global $wgGroupPermissions, $wgClaimWikiEmailTo, $wgClaimWikiEnabled, $wgEmergencyContact, $wgReverbNotifications;
+		global $wgGroupPermissions, $wgClaimWikiEmailTo, $wgClaimWikiEnabled,
+		$wgEmergencyContact, $wgReverbNotifications;
 
 		if (!isset($wgClaimWikiEnabled)) {
 			$wgClaimWikiEnabled = true;
